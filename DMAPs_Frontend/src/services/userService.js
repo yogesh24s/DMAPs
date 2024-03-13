cb40fd46dc945fe93cd7496083c7867732d08259
yogesh24s <yogesh24.ds@gmail.com>
2024-03-13 23:21:31 +0530

    design and validation url

--- DMAPs_Frontend/src/services/userService.js
+++ DMAPs_Frontend/src/services/userService.js
@@ -7,19 +7,19 @@ import api from "./interceptor";
 class userService {
 
     saveCompanyUsers(payload) {
-      return api.post(`http://127.0.0.1:4000/api/saveCompanyUsers`, payload);
+      return api.post(`http://3.92.91.120:4000/api/saveCompanyUsers`, payload);
     }
     editCompanyUsers(payload) {
-      return api.post(`http://127.0.0.1:4000/api/editCompanyUsers`, payload);
+      return api.post(`http://3.92.91.120:4000/api/editCompanyUsers`, payload);
     }
 
     deleteCompanyUsers(payload) {
-      return api.post(`http://127.0.0.1:4000/api/deleteCompanyUsers`, payload);
+      return api.post(`http://3.92.91.120:4000/api/deleteCompanyUsers`, payload);
     }
 
     getCompanyUsers(){
         return api
-      .get("http://127.0.0.1:4000/api/getCompanyUsers")
+      .get("http://3.92.91.120:4000/api/getCompanyUsers")
       .then((response) => {
         return response;
       })
