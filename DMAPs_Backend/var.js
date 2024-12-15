4b4c10aea2e335f9491ab7b4a82e845b34d3691c
yogesh24s <yogesh24.ds@gmail.com>
2024-12-15 07:49:46 +0530

    DB PWD

--- DMAPs_Backend/var.js
+++ DMAPs_Backend/var.js
@@ -8,7 +8,7 @@ var DMAPFunc = function() {
     // mysqlHost = '3.92.91.120';
     mysqlHost = '127.0.0.1';
 
-    mysqlPwd = 'test123';
+    mysqlPwd = 'affine123';
     // mysqlPwd = '@ff19E$P';
  
     // knex environemt
