c8f4b60efda7c0a3ac85d01e33da05a262d2fc22
Gourav199 <tewarygourav1997@gmail.com>
2024-12-16 19:22:27 +0530

    minor changes

--- DMAPs_Frontend/src/components/LandingLayOuts/StyleStore/StyleStoreLayOuts/PO_Details/OrderModal.js
+++ DMAPs_Frontend/src/components/LandingLayOuts/StyleStore/StyleStoreLayOuts/PO_Details/OrderModal.js
@@ -73,27 +73,36 @@ const OrderModal = ({
 		<Modal.Body>
 			<form onSubmit={handlePODetails}>
 				<div className='row'>
-					<div className='col-3' >
-						<InputGroup className="mb-3" style={{ display: 'flex', alignItems: 'center' }}>
-							<div style={{ width: '70%', paddingRight: '5px', marginTop: '-13px' }}>
-								Dmaps No
-							</div>
-							<Form.Select className='mb-3' tabindex="1" label='DMAPS No.' onChange={(e) => {
-								setStyleNo(e.target.value);
-								handleSizeGrid(e.target.value)
-							}} value={styleNo} name='styleNo' style={{ width: '100%' }}>
-								<option> Select DMAPS No. </option>
+					
+					<div className="col-4">
+						{/* Label and Input */}
+						<InputGroup className="mb-3">
+							<div style={{ marginBottom: '5px' }} className='me-5 mt-2'>Dmaps No</div>
+							<Form.Select
+								tabIndex="1"
+								onChange={(e) => {
+									setStyleNo(e.target.value);
+									handleSizeGrid(e.target.value);
+								}}
+								value={styleNo}
+								name="styleNo"
+								className="form-select"
+							>
+								<option>Select DMAPS No.</option>
 								{styleNoList.map((item) => (
 									<option key={item.Style_No} value={item.Style_No}>
 										{item.Style_No}
 									</option>
 								))}
 							</Form.Select>
-							{styleNoError && <p style={{ color: 'red' }}>{styleNoError}</p>}
 						</InputGroup>
 
-
+						{/* Error Message */}
+						{styleNoError && (
+							<div className="text-danger text-center" style={{ marginLeft:'150px'}}>{styleNoError}</div>
+						)}
 					</div>
+
 				</div>
 				{styleNo ? (
 					<>
@@ -164,154 +173,258 @@ const OrderModal = ({
 				) : null}
 				<h6 className='h6'> Order Details </h6>
 				<div className="row g-3">
-  {/* F PO No. */}
-  <div className="col-md-4 d-flex align-items-center">
-    <label className="form-label me-2" style={{ whiteSpace: "nowrap" }}>
-      F PO No.
-    </label>
-    <MDBInput
-      type="text"
-      placeholder="Enter F PO No."
-      value={fPONo}
-      onChange={(e) => setFPONo(e.target.value)}
-      name="fPONo"
-      className="form-control"
-    />
-  </div>
+					{/* F PO No. */}
+					<div className="col-md-4">
+						<div className="d-flex align-items-center mb-1">
+							<label className="form-label me-2" style={{ whiteSpace: "nowrap" }}>
+								F PO No.
+							</label>
+							<MDBInput
+								type="text"
+								placeholder="Enter F PO No."
+								value={fPONo}
+								onChange={(e) => setFPONo(e.target.value)}
+								name="fPONo"
+								className="form-control"
+							/>
+						</div>
+						{fPONoError && <div className="text-danger text-center">{fPONoError}</div>}
+					</div>
 
-  {/* Order No. */}
-  <div className="col-md-4 d-flex align-items-center">
-    <label className="form-label me-2" style={{ whiteSpace: "nowrap" }}>
-      Order No.
-    </label>
-    <MDBInput
-      type="text"
-      placeholder="Enter Order No."
-      value={PONo}
-      onChange={(e) => setPONo(e.target.value)}
-      name="PONo"
-      className="form-control"
-    />
-  </div>
+					{/* Order No. */}
+					<div className="col-md-4">
+						<div className="d-flex align-items-center mb-1">
+							<label className="form-label me-2" style={{ whiteSpace: "nowrap" }}>
+								Order No.
+							</label>
+							<MDBInput
+								type="text"
+								placeholder="Enter Order No."
+								value={PONo}
+								onChange={(e) => setPONo(e.target.value)}
+								name="PONo"
+								className="form-control"
+							/>
+						</div>
+						{PONoError && <div className="text-danger text-center">{PONoError}</div>}
+					</div>
 
-  {/* OC No. */}
-  <div className="col-md-4 d-flex align-items-center">
-    <label className="form-label me-2" style={{ whiteSpace: "nowrap" }}>
-      OC No.
-    </label>
-    <MDBInput
-      type="text"
-      placeholder="Enter OC No."
-      value={OCNo}
-      onChange={(e) => setOCNo(e.target.value)}
-      name="OCNo"
-      className="form-control"
-    />
-  </div>
+					{/* Emb. Type */}
+					<div className="col-md-4">
+						<div className="d-flex align-items-center mb-1">
+							<label className="form-label me-2" style={{ whiteSpace: "nowrap" }}>
+								Emb. Type
+							</label>
+							<Form.Select
+								value={embType}
+								onChange={(e) => setEmbType(e.target.value)}
+								name="embType"
+								className="form-select"
+							>
+								<option>Select Emb Type</option>
+								{embTypeList.map((item) => (
+									<option key={item.id} value={item.Emb_Type}>
+										{item.Emb_Type}
+									</option>
+								))}
+							</Form.Select>
+						</div>
+						{embTypeError && <div className="text-danger text-center">{embTypeError}</div>}
+					</div>
 
-  {/* Emb. Type */}
-  <div className="col-md-4 d-flex align-items-center">
-    <label className="form-label me-2" style={{ whiteSpace: "nowrap" }}>
-      Emb. Type
-    </label>
-    <Form.Select
-      value={embType}
-      onChange={(e) => setEmbType(e.target.value)}
-      name="embType"
-      className="form-select"
-    >
-      <option>Select Emb Type</option>
-      {embTypeList.map((item) => (
-        <option key={item.id} value={item.Emb_Type}>
-          {item.Emb_Type}
-        </option>
-      ))}
-    </Form.Select>
-  </div>
+					{/* Print Type */}
+					<div className="col-md-4">
+						<div className="d-flex align-items-center mb-1">
+							<label className="form-label me-2" style={{ whiteSpace: "nowrap" }}>
+								Print Type
+							</label>
+							<Form.Select
+								value={printType}
+								onChange={(e) => setPrintType(e.target.value)}
+								name="printType"
+								className="form-select"
+							>
+								<option>Select Print Type</option>
+								{printTypeList.map((item) => (
+									<option key={item.id} value={item.Print_Type}>
+										{item.Print_Type}
+									</option>
+								))}
+							</Form.Select>
+						</div>
+						{printTypeError && <div className="text-danger text-center">{printTypeError}</div>}
+					</div>
 
-  {/* Print Type */}
-  <div className="col-md-4 d-flex align-items-center">
-    <label className="form-label me-2" style={{ whiteSpace: "nowrap" }}>
-      Print Type
-    </label>
-    <Form.Select
-      value={printType}
-      onChange={(e) => setPrintType(e.target.value)}
-      name="printType"
-      className="form-select"
-    >
-      <option>Select Print Type</option>
-      {printTypeList.map((item) => (
-        <option key={item.id} value={item.Print_Type}>
-          {item.Print_Type}
-        </option>
-      ))}
-    </Form.Select>
-  </div>
+					{/* Washing Type */}
+					<div className="col-md-4">
+						<div className="d-flex align-items-center mb-1">
+							<label className="form-label me-2" style={{ whiteSpace: "nowrap" }}>
+								Washing Type
+							</label>
+							<Form.Select
+								value={washingType}
+								onChange={(e) => setWashingType(e.target.value)}
+								name="washingType"
+								className="form-select"
+							>
+								<option>Select Washing Type</option>
+								{washingTypeList.map((item) => (
+									<option key={item.id} value={item.Washing_Type}>
+										{item.Washing_Type}
+									</option>
+								))}
+							</Form.Select>
+						</div>
+						{washingTypeError && <div className="text-danger text-center ms-5 ps-5">{washingTypeError}</div>}
+					</div>
 
-  {/* Washing Type */}
-  <div className="col-md-4 d-flex align-items-center">
-    <label className="form-label me-2" style={{ whiteSpace: "nowrap" }}>
-      Washing Type
-    </label>
-    <Form.Select
-      value={washingType}
-      onChange={(e) => setWashingType(e.target.value)}
-      name="washingType"
-      className="form-select"
-    >
-      <option>Select Washing Type</option>
-      {washingTypeList.map((item) => (
-        <option key={item.id} value={item.Washing_Type}>
-          {item.Washing_Type}
-        </option>
-      ))}
-    </Form.Select>
-  </div>
+					{/* Delivery Date */}
+					<div className="col-md-4">
+						<div className="d-flex align-items-center mb-1">
+							<label className="form-label me-2" style={{ whiteSpace: "nowrap" }}>
+								Delivery Date
+							</label>
+							<DatePicker
+								selected={deliveryDate}
+								onChange={(date) => setDeliveryDate(date)}
+								className="form-control"
+								dateFormat="dd/MM/yyyy"
+								placeholderText="dd/mm/yyyy"
+							/>
+						</div>
+						{deliveryDateError && <div className="text-danger text-center ms-5 ps-5">{deliveryDateError}</div>}
+					</div>
 
-  {/* Delivery Date */}
-  <div className="col-md-4 d-flex align-items-center">
-    <label className="form-label me-2" style={{ whiteSpace: "nowrap" }}>
-      Delivery Date
-    </label>
-    <DatePicker
-      selected={deliveryDate}
-      onChange={(date) => setDeliveryDate(date)}
-      className="form-control"
-      dateFormat="dd/MM/yyyy"
-      placeholderText="dd/mm/yyyy"
-    />
-  </div>
+					{/* PCD */}
+					<div className="col-md-4">
+						<div className="d-flex align-items-center mb-1">
+							<label className="form-label me-2" style={{ whiteSpace: "nowrap" }}>
+								PCD
+							</label>
+							<DatePicker
+								selected={pcd}
+								onChange={(date) => setPCD(date)}
+								className="form-control"
+								dateFormat="dd/MM/yyyy"
+								placeholderText="dd/mm/yyyy"
+							/>
+						</div>
+						{pcdError && <div className="text-danger text-center">{pcdError}</div>}
+					</div>
 
-  {/* PCD */}
-  <div className="col-md-4 d-flex align-items-center">
-    <label className="form-label me-2" style={{ whiteSpace: "nowrap" }}>
-      PCD
-    </label>
-    <DatePicker
-      selected={pcd}
-      onChange={(date) => setPCD(date)}
-      className="form-control"
-      dateFormat="dd/MM/yyyy"
-      placeholderText="dd/mm/yyyy"
-    />
-  </div>
+					{/* Shipment Mode */}
+					<div className="col-md-4">
+						<div className="d-flex align-items-center mb-1">
+							<label className="form-label me-2" style={{ whiteSpace: "nowrap" }}>
+								Shipment Mode
+							</label>
+							<MDBInput
+								type="text"
+								placeholder="Enter Shipment Mode"
+								value={shipmentMode}
+								onChange={(e) => setShipmentMode(e.target.value)}
+								name="shipmentMode"
+								className="form-control"
+							/>
+						</div>
+						{shipmentModeError && <div className="text-danger text-center  ms-5 ps-5">{shipmentModeError}</div>}
+					</div>
+					{/* Others */}
+					<div className="col-md-6">
+						<label className="form-label">Others</label>
+						<MDBInput
+							type="text"
+							placeholder="Enter Others"
+							value={others}
+							onChange={(e) => setOthers(e.target.value)}
+							name="others"
+						/>
+					</div>
 
-  {/* Shipment Mode */}
-  <div className="col-md-4 d-flex align-items-center">
-    <label className="form-label me-2" style={{ whiteSpace: "nowrap" }}>
-      Shipment Mode
-    </label>
-    <MDBInput
-      type="text"
-      placeholder="Enter Shipment Mode"
-      value={shipmentMode}
-      onChange={(e) => setShipmentMode(e.target.value)}
-      name="shipmentMode"
-      className="form-control"
-    />
-  </div>
-</div>
+					{/* Notes */}
+					<div className="col-md-6">
+						<label className="form-label">Notes</label>
+						<MDBInput
+							type="textarea"
+							placeholder="Enter Notes"
+							value={note}
+							onChange={(e) => setNote(e.target.value)}
+							name="note"
+							rows="3"
+						/>
+					</div>
+
+				</div>
+
+				<div className="col-12 mt-20 parentDivStyle mt-5">
+					{styleNo && sizesArray && sizesArray.length > 0 ? (
+						<>
+							<table border="1" className='table tableStyle'>
+								<thead>
+									<tr>
+										<th style={{ width: '100px' }}>Garment Color</th>
+										<th style={{ width: '100px' }}>Destination Country</th>
+										{sizesArray.map((sizeKey) => (
+											<th className='thTdStyle' key={sizeKey}>{sizeKey}</th>
+										))}
+										<th style={{ width: '50px' }}>Total</th>
+										<th style={{ width: '60px' }}>Actions</th>
+									</tr>
+								</thead>
+								<tbody>
+									{rows.map((row, rowIndex) => (
+										<tr key={rowIndex}>
+											<td className='thTdStyle'>
+												<input
+													type="color"
+													value={row.garmentColor || '#000000'}
+													onChange={(e) => {
+														const updatedRows = [...rows];
+														updatedRows[rowIndex].garmentColor = e.target.value;
+														setRows(updatedRows);
+													}}
+												/>
+											</td>
+											<td className='thTdStyle'>
+												<input
+													type="text"
+													value={row.destinationCountry}
+													onChange={(e) => {
+														const updatedRows = [...rows];
+														updatedRows[rowIndex].destinationCountry = e.target.value;
+														setRows(updatedRows);
+													}}
+												/>
+											</td>
+											{/* Dynamically generate size inputs */}
+											{sizesArray.map((sizeKey) => (
+												<td className='thTdStyle' key={sizeKey}>
+													<input
+														type="number"
+														value={row.sizes[sizeKey] || ''}
+														onChange={(e) => {
+															const updatedRows = [...rows];
+															updatedRows[rowIndex].sizes[sizeKey] = e.target.value;
+															updatedRows[rowIndex].total = calculateTotal(updatedRows[rowIndex].sizes); // Recalculate total
+															setRows(updatedRows);
+														}}
+													/>
+												</td>
+											))}
+											<td className='thTdStyle'>{row.total}</td>
+											<td className='thTdStyle'>
+												<i className='fa fa-trash ml-15 pointer' onClick={() => handleDeleteRow(rowIndex)} title='Delete'> </i>
+											</td>
+										</tr>
+									))}
+								</tbody>
+							</table>
+							<Button variant="success" onClick={addNewRow} style={{ float: 'right', width: '5%', marginTop: "-20PX" }}> <i className='fa fa-plus fa-1x white'> </i>   </Button>
+						</>
+					) : null}
+
+				</div>
+
 
 
 
